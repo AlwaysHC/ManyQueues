e7b372c85ea904cc30f87c0e7acfca6e8e7265fe
Mirko Sacripanti <mirko-github@nextware.it>
2020-07-19 16:54:02 +0200

    New Write/Read conf methods

--- Manager.cs
+++ Manager.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
 
 #nullable enable
 
@@ -6,8 +8,10 @@ namespace NW.ManyQueues {
     public interface IManager {
         public void WriteConf(string name, string value);
         public void WriteConf<TValue>(string name, TValue value);
+        public void WriteConf<TValue>(Expression<Func<TValue>> expr);
         public string ReadConf(string name);
         public TValue ReadConf<TValue>(string name);
+        public TValue ReadConf<TValue>(Expression<Func<TValue>> expr);
     }
 
     public class Manager: IManager {
@@ -21,12 +25,30 @@ namespace NW.ManyQueues {
             return (TValue)_Conf[name];
         }
 
+        public TValue ReadConf<TValue>(Expression<Func<TValue>> expr) {
+            if (expr.Body is MemberExpression CorpoExpr) {
+                return ReadConf<TValue>(CorpoExpr.Member.Name);
+            }
+            else {
+                return default!;
+            }
+        }
+
         public void WriteConf(string name, string value) {
             _Conf[name] = value;
         }
 
         public void WriteConf<TValue>(string name, TValue value) {
             _Conf[name] = value!;
+        }
+
+        public void WriteConf<TValue>(Expression<Func<TValue>> expr) {
+            if (expr != null) {
+                Func<TValue>? CalcolaExpr = expr.Compile();
+                if (expr.Body is MemberExpression CorpoExpr) {
+                    WriteConf<TValue>(CorpoExpr.Member.Name, CalcolaExpr());
+                }
+            }
         }
     }
 
@@ -47,12 +69,20 @@ namespace NW.ManyQueues {
             _Manager.WriteConf(name, value);
         }
 
+        public void WriteConf<TValue>(Expression<Func<TValue>> expr) {
+            _Manager.WriteConf<TValue>(expr);
+        }
+
         public string ReadConf(string name) {
             return _Manager.ReadConf(name);
         }
 
         public TValue ReadConf<TValue>(string name) {
             return _Manager.ReadConf<TValue>(name);
+        }
+
+        public TValue ReadConf<TValue>(Expression<Func<TValue>> expr) {
+            return _Manager.ReadConf<TValue>(expr);
         }
     }
 }
